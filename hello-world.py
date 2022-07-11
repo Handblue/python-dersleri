e45df2124e03925b36d7f1655c8f6845b5a53b0a
Handblue <71235117+Handblue@users.noreply.github.com>
2022-07-11 11:00:23 +0300

    hellooo

---
+++ hello-world.py
@@ -0,0 +1,46 @@
+from winreg import REG_CREATED_NEW_KEY
+
+
+print("merhaba \ndunya")
+print("merhaba \tdunya")
+ 
+# n yazılan alta inme  t ise tab boşluğu
+
+print("benim adım {}, yasim {}".format("emir", 22))
+
+print("benim adım {0}, yasim {1}".format("emir", 22))
+
+
+print("benim adım {ad}, yasim {yas}".format(ad="emir", yas=22))
+
+print(10+3)
+
+# değişkenler
+
+sayi = 10
+print(sayi) 
+
+sayi = sayi +1 
+print(sayi)
+
+
+# veri topleri
+#
+#Tip             kısatlam               örnek 
+#
+#---------------------------------------------
+#integer         int                     3, 10
+#
+#float           float                   1.5, 7.0
+#
+#string          str                     "slm", '2'
+#
+#boolean         bool                    True, False
+#
+#list            list                    [1,2,True,'a',1] 
+#
+#set             set                     {1,2,True}
+#
+#dictionary      dict                    {'isim': 'emir', 'yas': 22}
+#
+#tuple           tup                     (1, 2, True)                   
