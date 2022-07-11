9a27a6551d9cd96bd53f949fcb55abfd619dfe2f
Handblue <71235117+Handblue@users.noreply.github.com>
2022-07-11 10:54:47 +0300

    veri tipleri

---
+++ kısaca-bool-str-list-tup/boolean.py
@@ -0,0 +1,14 @@
+a = True 
+b = False 
+print(a)
+print(b)
+c = 'Ture'
+print(type(c)) #type kullanarak değişkenimizin tipini gösterdik
+
+
+yas1 = 20
+yas2 = 18
+
+print(yas2> 18)
+print(yas2 == 18)
+print(yas2 != 18)
