d1f82205f3e2b08fba0aef930ac90301ba56d548
Handblue <71235117+Handblue@users.noreply.github.com>
2022-07-11 10:49:50 +0300

    obje - veri yapıları

---
+++ detayli-veriyapilari/strings.py
@@ -0,0 +1,21 @@
+'''
+name = 'emir'
+surname = 'yigit'
+age = 22
+
+print('my name is ' + name + ' ' + surname + ' and \nI am ' + str(age)+ ' years old.')
+
+'''
+# farklı yoldan yapımı 
+
+'''
+
+name = 'emir'
+surname = 'yigit'
+age = 22
+
+greeting = 'my name is ' + name + ' ' + surname + ' and \nI am ' + str(age)+ ' years old.'
+
+print(greeting)
+
+'''
