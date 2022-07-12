9ffafb55881ff7cb00dd91e7e881f10052aad351
Emir Yiğit <71235117+Handblue@users.noreply.github.com>
2022-07-12 13:27:17 +0300

    listeleme

---
+++ detayli-veriyapilari/lists.py
@@ -0,0 +1,24 @@
+from email import message
+import numbers
+
+
+# message = ' Hello there. My name is Emir yigit'.split()
+# print(message)
+
+
+# my_list = [1,2,3]
+# my_list = ['bir',2, True, 5.6]
+# print(my_list)
+
+
+# list1 = ['one','two','there']
+# list2 = ['four','five','six']
+# numbers = list1 + list2
+# print(numbers)
+# print(len(numbers))
+
+
+# userA = ['Emir',22]
+# userB = ['Osman',12]
+# print(userA)
+# print(userB)
