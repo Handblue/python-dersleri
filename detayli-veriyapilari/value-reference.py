0e7f151f58c36abfc2d44ac0fd2c8fb434be24f2
Emir Yiğit <71235117+Handblue@users.noreply.github.com>
2022-07-16 17:00:00 +0300

    value-reference

---
+++ detayli-veriyapilari/value-reference.py
@@ -0,0 +1,23 @@
+from tkinter import Y
+
+
+# x = 5
+# y = 25
+
+# x = y 
+
+# y = 10  # y değişkenini 10 olarak güncelledik
+
+# print(x,y)
+
+#reference type
+
+a = ["apple","banana"]
+b = ["apple","banana"]
+
+a = b
+
+b[0] = "grape"   # sıfırıncı index üzerine işlendi ve apple yerine grape yazıldı.
+
+print(a,b)
+
