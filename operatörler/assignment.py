499ed117e0a064dbaf064403bd62969e72c3a8f9
Emir Yiğit <71235117+Handblue@users.noreply.github.com>
2022-07-16 17:15:36 +0300

    operatörler

---
+++ operatörler/assignment.py
@@ -0,0 +1,26 @@
+# x = 10
+# y = 20
+# z = 30 
+
+# iki farklı şekilde de kullanılabilir.  x, y, z = 10, 20, 30  
+
+x, y, z = 10, 20, 30      
+
+#x = x + 5   # x değeri üstüne 5 ekledi.
+x += 5       # aynısı farklı yoldan
+x -= 5       # çıkartma
+x *= 5       # çarpma
+x /= 5       # bölme
+x //= 5      # tam bölme
+x %= 5       # mod
+
+#print(x, y, z)
+
+values = 1, 2, 3
+
+print(values)
+print(type(values))
+
+x, y, z = values
+
+print(x, y, z)
